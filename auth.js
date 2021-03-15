4b552fdcf17cada1cf89cce4796ba0c3cd5987ca
benjamin yee <benjamin.yee@ucdenver.edu>
2021-03-14 23:43:51 -0600

    new stuff

--- auth.js
+++ auth.js
@@ -1,18 +1,18 @@
+// Load required packages
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
-
+var User = require('./Schema/User')
 passport.use(new BasicStrategy(
-   function(username, password, done) {
-       var user = { name: "cu_user"}; //could have called to a database to look this up
-       if (username === user.name && password === "cu_rulez") // tripple equal is type and value; double == is just equal
-       {
-           return done(null, user);
-       }
-       else
-       {
-           return done(null, false);
-       }
-   }
+    function(username, password, done) {
+        //hard coded
+
+        User.findOne({username: username,password: password},function(err,result){
+            if(!result)  return done(null, false);
+            else
+                return done(null, result.username);
+
+        })
+    }
 ));
 
-exports.isAuthenticated = passport.authenticate('basic', { session: false });
+exports.isAuthenticated = passport.authenticate('basic', { session : false });
--- db.js
+++ db.js
@@ -16,7 +16,7 @@ module.exports = function () {
             return 1;
         },
         /*
-         * Retrieve a movie with a given id or return all the movies if the id is undefined.
+         * Retrieve a user with a given id or return all user if the id is undefined.
          */
         find: function (id) {
             if (id) {
@@ -30,7 +30,7 @@ module.exports = function () {
         },
         findOne: function (name) {
             if (name) {
-                return this.userList.find(function (element) {
+                return this.userList.find(function (element){
                     return element.username === name;
                 });
             }
@@ -39,7 +39,7 @@ module.exports = function () {
             }
         },
         /*
-         * Delete a movie with the given id.
+         * Delete a user with the given id.
          */
         remove: function (id) {
             var found = 0;
@@ -54,7 +54,7 @@ module.exports = function () {
             return found;
         },
         /*
-         * Update a movie with the given id
+         * Update a user with the given id
          */
         update: function (id, user) {
             var userIndex = this.userList.findIndex(function (element) {
